5b650cac4cf5bfa930a6acc49b6aa862f3653b94
Jordi Gil <jgil@redhat.com>
2025-11-10 13:51:26 -0500

    revert: Revert all changes to baseline - test upstream code first
    - Revert view.py CACHE_RH_IDENTITY_HEADER() fix
    - Revert feature_flags.py UNLEASH_DISABLED changes
    - Will add changes back one at a time to identify performance issue

--- koku/api/report/view.py
+++ koku/api/report/view.py
@@ -50,7 +50,7 @@ class ReportView(APIView):
     It providers one GET endpoint for the reports.
     """
 
-    @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER()))  # Call function to get header name
+    @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
     def get(self, request, **kwargs):
         """Get Report Data.
 
